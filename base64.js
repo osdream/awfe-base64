67bd536d978c1e8e7445a12e41b97dde2a5d44c4
songao <songao@zenmen.com>
2015-10-08 20:20:10 +0800

    fix

--- base64.js
+++ base64.js
@@ -129,7 +129,8 @@
             _utf8_decode: function(utftext) {
                 var string = "";
                 var i = 0;
-                var c = c1 = c2 = 0;
+                var c, c1, c2, c3;
+                c = c1 = c2 = 0;
 
                 while (i < utftext.length) {
 
